5fe9a3883996087e421933616779c766ddd238da
BuddistCoding <jason90255@gmail.com>
2022-08-31 07:24:58 +0000

    from 87

---
+++ espnet/nets/pytorch_backend/e2e_asr_improved_maskctc.py
@@ -0,0 +1,408 @@
+# Copyright 2020 Johns Hopkins University (Shinji Watanabe)
+#                Waseda University (Yosuke Higuchi)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+"""
+Mask CTC based non-autoregressive speech recognition model (pytorch).
+
+See https://arxiv.org/abs/2005.08700 for the detail.
+
+"""
+
+from itertools import groupby
+import logging
+import math
+
+from distutils.util import strtobool
+import numpy
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import chainer
+from chainer import reporter
+
+
+from espnet.nets.pytorch_backend.conformer.encoder import Encoder
+from espnet.nets.pytorch_backend.conformer.argument import (
+    add_arguments_conformer_common,  # noqa: H301
+)
+from espnet.nets.pytorch_backend.e2e_asr import CTC_LOSS_THRESHOLD
+from espnet.nets.pytorch_backend.e2e_asr_transformer import E2E as E2ETransformer
+# from espnet.nets.pytorch_backend.e2e_asr_conformer import E2E as E2EConformer
+from espnet.nets.pytorch_backend.transformer.decoder import Decoder
+from espnet.nets.pytorch_backend.maskctc.add_mask_token import mask_uniform, mask_uniform_dlp, shrink, insert_mask, expand
+from espnet.nets.pytorch_backend.maskctc.mask import square_mask
+from espnet.nets.pytorch_backend.nets_utils import make_non_pad_mask
+from espnet.nets.pytorch_backend.nets_utils import th_accuracy
+from espnet.nets.pytorch_backend.transformer.label_smoothing_loss import (
+    LabelSmoothingLoss    # noqa: H301
+)
+
+
+class DLPReporter(chainer.Chain):
+    def report(self, loss_ctc, loss_att, acc, loss_dlp, acc_dlp , cer_ctc, cer, wer, mtl_loss):
+        reporter.report({"loss_ctc": loss_ctc}, self)
+        reporter.report({"loss_att": loss_att}, self)
+        reporter.report({"acc": acc}, self)
+        reporter.report({"loss_dlp": loss_dlp}, self)
+        reporter.report({"acc_dlp": acc_dlp}, self)
+        reporter.report({"cer_ctc": cer_ctc}, self)
+        reporter.report({"cer": cer}, self)
+        reporter.report({"wer": wer}, self)
+        logging.info("mtl loss" + str(mtl_loss))
+        reporter.report({"mtl_loss": mtl_loss}, self)
+        
+
+
+class E2E(E2ETransformer):
+    """E2E module.
+
+    :param int idim: dimension of inputs
+    :param int odim: dimension of outputs
+    :param Namespace args: argument Namespace containing options
+
+    """
+
+    @staticmethod
+    def add_arguments(parser):
+        """Add arguments."""
+        E2ETransformer.add_arguments(parser)
+        E2E.add_maskctc_arguments(parser)
+        # E2EConformer.add_arguments(parser)
+        return parser
+
+    @staticmethod
+    def add_maskctc_arguments(parser):
+        """Add arguments for maskctc model."""
+        group = parser.add_argument_group("maskctc specific setting")
+
+        group.add_argument(
+            "--maskctc-use-conformer-encoder",
+            default=False,
+            type=strtobool,
+        )
+        group = add_arguments_conformer_common(group)
+        
+        # for imporved mask-ctc
+        group.add(
+            "--mtlbeta",
+            default=0.0,
+            type=float,
+        )
+        # end
+
+        return parser
+
+    def __init__(self, idim, odim, args, ignore_id=-1):
+        """Construct an E2E object.
+
+        :param int idim: dimension of inputs
+        :param int odim: dimension of outputs
+        :param Namespace args: argument Namespace containing options
+        """
+        odim += 1  # for the mask token
+        
+        self.mtlbeta = args.mtlbeta
+
+        super().__init__(idim, odim, args, ignore_id)
+        assert 0.0 <= self.mtlalpha < 1.0, "mtlalpha should be [0.0, 1.0)"
+        assert 0.0 <= self.mtlbeta, "mtlbeta should be non-negative"
+
+        self.mask_token = odim - 1
+        self.sos = odim - 2
+        self.eos = odim - 2
+        self.odim = odim
+
+        if args.maskctc_use_conformer_encoder:
+            if args.transformer_attn_dropout_rate is None:
+                args.transformer_attn_dropout_rate = args.conformer_dropout_rate
+            self.encoder = Encoder(
+                idim=idim,
+                attention_dim=args.adim,
+                attention_heads=args.aheads,
+                linear_units=args.eunits,
+                num_blocks=args.elayers,
+                input_layer=args.transformer_input_layer,
+                dropout_rate=args.dropout_rate,
+                positional_dropout_rate=args.dropout_rate,
+                attention_dropout_rate=args.transformer_attn_dropout_rate,
+                pos_enc_layer_type=args.transformer_encoder_pos_enc_layer_type,
+                selfattention_layer_type=args.transformer_encoder_selfattn_layer_type,
+                activation_type=args.transformer_encoder_activation_type,
+                macaron_style=args.macaron_style,
+                use_cnn_module=args.use_cnn_module,
+                cnn_module_kernel=args.cnn_module_kernel,
+            )
+        self.decoder = Decoder(
+                odim=odim,
+                selfattention_layer_type=args.transformer_decoder_selfattn_layer_type,
+                attention_dim=args.adim,
+                attention_heads=args.aheads,
+                conv_wshare=args.wshare,
+                conv_kernel_length=args.ldconv_decoder_kernel_length,
+                conv_usebias=args.ldconv_usebias,
+                linear_units=args.dunits,
+                num_blocks=args.dlayers,
+                dropout_rate=args.dropout_rate,
+                positional_dropout_rate=args.dropout_rate,
+                self_attention_dropout_rate=args.transformer_attn_dropout_rate,
+                src_attention_dropout_rate=args.transformer_attn_dropout_rate,
+                use_output_layer = False,
+        )
+        self.reset_parameters(args)
+        self.decoder_output_layer = torch.nn.Linear(args.adim, odim)
+        self.dlp = nn.Linear(args.adim, 50) # What number?
+        self.dlp_criterion = LabelSmoothingLoss(50, self.ignore_id, 0.0, False)
+        self.reporter = DLPReporter()
+
+        self.reset_parameters(args)
+
+    def forward(self, xs_pad, ilens, ys_pad):
+        """E2E forward.
+
+        :param torch.Tensor xs_pad: batch of padded source sequences (B, Tmax, idim)
+        :param torch.Tensor ilens: batch of lengths of source sequences (B)
+        :param torch.Tensor ys_pad: batch of padded target sequences (B, Lmax)
+        :return: ctc loss value
+        :rtype: torch.Tensor
+        :return: attention loss value
+        :rtype: torch.Tensor
+        :return: accuracy in attention decoder
+        :rtype: float
+        """
+
+        # 1. forward encoder
+        xs_pad = xs_pad[:, : max(ilens)]  # for data parallel
+        src_mask = make_non_pad_mask(ilens.tolist()).to(xs_pad.device).unsqueeze(-2)
+        hs_pad, hs_mask = self.encoder(xs_pad, src_mask)
+        self.hs_pad = hs_pad
+
+        # 2. forward decoder
+        ys_in_pad, ys_out_pad = mask_uniform(
+            ys_pad, self.mask_token, self.eos, self.ignore_id
+        )
+        ys_mask = square_mask(ys_in_pad, self.eos)
+        pred_pad, pred_mask = self.decoder(ys_in_pad, ys_mask, hs_pad, hs_mask)
+        pred_pad = self.decoder_output_layer(pred_pad) ## important(why ??)
+        self.pred_pad = pred_pad
+
+
+        # 3. compute attention loss
+        loss_att = self.criterion(pred_pad, ys_out_pad)
+        self.acc = th_accuracy(
+            pred_pad.view(-1, self.odim), ys_out_pad, ignore_label=self.ignore_id
+        )
+
+        # 4. compute insertion / deletion error
+        ys_in_pad_dlp, ys_out_pad_dlp = mask_uniform_dlp(
+            ys_pad, self.mask_token, self.eos, self.ignore_id
+        )
+        ys_mask_dlp = square_mask(ys_in_pad_dlp, self.eos)
+        pred_pad_dlp, _ = self.decoder(ys_in_pad_dlp, ys_mask_dlp, hs_pad.repeat(2,1,1), hs_mask.repeat(2,1,1))
+        pred_pad_dlp = self.dlp(pred_pad_dlp)
+       
+        self.loss_dlp = self.dlp_criterion(pred_pad_dlp, ys_out_pad_dlp)
+        self.acc_dlp = th_accuracy(
+            pred_pad_dlp.view(-1, 50), ys_out_pad_dlp, ignore_label = self.ignore_id
+        )
+        
+        # 5. compute ctc loss
+        loss_ctc, cer_ctc = None, None
+        if self.mtlalpha > 0:
+            batch_size = xs_pad.size(0)
+            hs_len = hs_mask.view(batch_size, -1).sum(1)
+            loss_ctc = self.ctc(hs_pad.view(batch_size, -1, self.adim), hs_len, ys_pad)
+            if self.error_calculator is not None:
+                ys_hat = self.ctc.argmax(hs_pad.view(batch_size, -1, self.adim)).data
+                cer_ctc = self.error_calculator(ys_hat.cpu(), ys_pad.cpu(), is_ctc=True)
+            # for visualization
+            if not self.training:
+                self.ctc.softmax(hs_pad)
+
+        
+
+
+        # if (self.mtlbeta > 0):
+        #     batch_size = xs_pad.size(0)
+        #     hs_len = hs_mask.view(batch_size, -1).sum(1)
+        #     loss_ctc = self.ctc(hs_pad.view(batch_size, -1, self.adim), hs_len, ys_pad)
+            
+        # 6. compute cer/wer
+        if self.training or self.error_calculator is None or self.decoder is None:
+            cer, wer = None, None
+        else:
+            ys_hat = pred_pad.argmax(dim=-1)
+            cer, wer = self.error_calculator(ys_hat.cpu(), ys_pad.cpu())
+
+        alpha = self.mtlalpha
+        beta = self.mtlbeta
+        if alpha == 0:
+            self.loss = loss_att
+            loss_att_data = float(loss_att)
+            loss_ctc_data = None
+        else:
+            self.loss = alpha * loss_ctc + (1 - alpha) * loss_att
+            loss_att_data = float(loss_att)
+            loss_ctc_data = float(loss_ctc)
+
+        self.loss =  (self.loss + beta * self.loss_dlp)
+
+        loss_dlp_data = float(self.loss_dlp)
+
+        loss_data = float(self.loss)
+        if loss_data < CTC_LOSS_THRESHOLD and not math.isnan(loss_data):
+            self.reporter.report(
+                loss_ctc_data, loss_att_data, self.acc, loss_dlp_data, self.acc_dlp, cer_ctc, cer, wer, loss_data
+            )
+        else:
+            logging.warning("loss (=%f) is not correct", loss_data)
+        return self.loss
+    
+    def recognize(self, x, recog_args, char_list=None, rnnlm=None):
+        """Recognize input speech.
+
+        :param ndnarray x: input acoustic feature (B, T, D) or (T, D)
+        :param Namespace recog_args: argment Namespace contraining options
+        :param list char_list: list of characters
+        :param torch.nn.Module rnnlm: language model module
+        :return: decoding result
+        :rtype: list
+        """
+
+        def num2str(char_list, mask_token, mask_char="_"):
+            def f(yl):
+                cl = [char_list[y] if y != mask_token else mask_char for y in yl]
+                return "".join(cl).replace("<space>", " ")
+
+            return f
+
+        n2s = num2str(char_list, self.mask_token)
+
+        self.eval()
+        h = self.encode(x).unsqueeze(0)
+
+        # greedy ctc outputs
+        ctc_probs, ctc_ids = torch.exp(self.ctc.log_softmax(h)).max(dim=-1)
+        y_hat = torch.stack([x[0] for x in groupby(ctc_ids[0])])
+        y_idx = torch.nonzero(y_hat != 0).squeeze(-1)
+
+        # calculate token-level ctc probabilities by taking
+        # the maximum probability of consecutive frames with
+        # the same ctc symbols
+        probs_hat = []
+        cnt = 0
+        for i, y in enumerate(y_hat.tolist()):
+            probs_hat.append(-1)
+            while cnt < ctc_ids.shape[1] and y == ctc_ids[0][cnt]:
+                if probs_hat[i] < ctc_probs[0][cnt]:
+                    probs_hat[i] = ctc_probs[0][cnt].item()
+                cnt += 1
+        probs_hat = torch.from_numpy(numpy.array(probs_hat))
+
+        # mask ctc outputs based on ctc probabilities
+        p_thres = recog_args.maskctc_probability_threshold
+        mask_idx = torch.nonzero(probs_hat[y_idx] < p_thres).squeeze(-1)
+        confident_idx = torch.nonzero(probs_hat[y_idx] >= p_thres).squeeze(-1)
+        mask_num = len(mask_idx)
+
+        y_in = torch.zeros(1, len(y_idx), dtype=torch.long) + self.mask_token
+        y_in[0][confident_idx] = y_hat[y_idx][confident_idx]
+
+        logging.info("ctc:{}".format(n2s(y_in[0].tolist())))
+
+# =========== origin iterative decoding ==========
+
+        # iterative decoding
+        # if not mask_num == 0:
+        #     K = recog_args.maskctc_n_iterations
+        #     num_iter = K if mask_num >= K and K > 0 else mask_num
+
+        #     for t in range(num_iter - 1):
+        #         pred, _ = self.decoder(y_in, None, h, None)
+        #         pred_score, pred_id = pred[0][mask_idx].max(dim=-1)
+        #         cand = torch.topk(pred_score, mask_num // num_iter, -1)[1]
+        #         y_in[0][mask_idx[cand]] = pred_id[cand]
+        #         mask_idx = torch.nonzero(y_in[0] == self.mask_token).squeeze(-1)
+
+        #         logging.info("msk:{}".format(n2s(y_in[0].tolist())))
+
+        #     # predict leftover masks (|masks| < mask_num // num_iter)
+        #     pred, pred_mask = self.decoder(y_in, None, h, None)
+        #     y_in[0][mask_idx] = pred[0][mask_idx].argmax(dim=-1)
+
+        #     logging.info("msk:{}".format(n2s(y_in[0].tolist())))
+
+# =========== origin iterative decoding ==========
+        
+        K = recog_args.maskctc_n_iterations
+        if K > 0:
+            num_decode = max( (mask_num // K) , 1)
+        else:
+            num_decode = 1
+
+        # shrink-and-expand algorithm
+        
+        logging.info(f"predict {num_decode} in each iteration.")
+
+        while len(mask_idx) > 0:
+            y_tmp = torch.arange(y_in.size(1)).to(y_in)
+            y_tmp[mask_idx] = -1
+            _, cnt = y_tmp.unique_consecutive(return_counts = True)
+            y_in = y_in[0][cnt.cumsum(0) - 1].unsqueeze(0)
+            mask_idx = torch.nonzero(y_in[0] == self.mask_token).squeeze(-1)
+            logging.info(f'shr:{n2s(y_in[0].tolist())}')
+
+            pred, _ = self.decoder(y_in, None, h, None)
+            pred_dlp = self.dlp_layer(pred)
+            _ , pred_len = pred_dlp[0].max(dim = -1)
+            y_tmp = [numpy.array([self.mask_token] * f.item()) if i in mask_idx else y_in[0][i].numpy() for i, f in enumerate(pred_len)]
+            y_tmp = torch.tensor(numpy.hstack(y_tmp)).unsqueeze(0)
+            y_in = y_tmp.long()
+            mask_idx = torch.nonzero(y_in[0] == self.mask_token).squeeze(-1)
+            logging.info(f"exp{n2s(y_in[0].tolist())}")
+
+            if (len(mask_idx) == 0):
+                break
+
+            pred, _ = self.decoder(y_in, None, h, None)
+            pred_y = self.decoder_output_layer(pred)
+            pred_y_score, pred_y_id = pred_y[0][mask_idx].max(dim=-1)
+            _, cand = torch.topk(pred_y_score, min(num_decode, len(mask_idx)), -1)
+            y_in[0][mask_idx[cand]] = pred_y_id[cand]
+            mask_idx = torch.nonzero(y_in[0] == self.mask_token).squeeze(-1)
+            logging.info(f'pre:{n2s(y_in[0].tolist())}')
+        
+        logging.info("------------")
+        logging.info(f"ctc:{n2s(y_hat[y_idx].tolist())}")
+        logging.info(f"msk:{n2s(y_in[0].tolist())}")
+        logging.info("------------")
+
+            # y_in, _ = shrink(y_in, self.mask_token, self.eos, self.ignore_id)
+            # shrink_idx = torch.nonzero(y_in == self.mask_token)
+            # y_dlp = torch.zeros(y_in.shape[1]).fill_(self.ignore_id).to(y_in)
+            # pred, _ = self.decoder(y_in, None, h, None)
+            # _ , dlp_length = self.dlp(pred)[0].max(dim = -1)
+            # y_dlp[shrink_idx] = dlp_length[shrink_idx]
+
+            # y_in = expand(y_in, y_dlp.unsqueeze(0), self.mask_token, self.eos, self.ignore_id)
+            # mask_idx = torch.nonzero(y_in[0] == self.mask_token).squeeze(-1)
+            
+            # logging.info(f'after_expand:{y_in}')
+            # if (len(mask_idx) == 0):
+            #     break
+            # mask_num = len(mask_idx)
+            # pred, _ = self.decoder(y_in, None, h, None)
+            # pred_score, pred_idx = pred[0][mask_idx].max(dim = -1)
+            # cand = torch.topk(pred_score, min(num_decode, len(mask_idx)), -1)[1]
+            
+            # y_in[0][mask_idx[cand]] = pred_idx[cand]
+
+            
+            # mask_idx == torch.nonzero(y_in[0] == self.mask_token)
+
+
+        ret = y_in.tolist()[0]
+        hyp = {"score": 0.0, "yseq": [self.sos] + ret + [self.eos]}
+
+        return [hyp]
--- espnet/nets/pytorch_backend/maskctc/add_mask_token.py
+++ espnet/nets/pytorch_backend/maskctc/add_mask_token.py
@@ -8,6 +8,8 @@
 """Token masking module for Masked LM."""
 
 import numpy
+import torch
+import logging
 
 
 def mask_uniform(ys_pad, mask_token, eos, ignore_id):
@@ -31,9 +33,139 @@ def mask_uniform(ys_pad, mask_token, eos, ignore_id):
     ys_in = [y.clone() for y in ys]
     for i in range(len(ys)):
         num_samples = numpy.random.randint(1, len(ys[i]) + 1)
-        idx = numpy.random.choice(len(ys[i]), num_samples)
+        idx = numpy.random.choice(len(ys[i]), num_samples, replace = False)
 
         ys_in[i][idx] = mask_token
         ys_out[i][idx] = ys[i][idx]
 
     return pad_list(ys_in, eos), pad_list(ys_out, ignore_id)
+
+
+def mask_uniform_dlp(ys_pad, mask_token, eos, ignore_id):
+    from espnet.nets.pytorch_backend.nets_utils import pad_list
+
+    ys = [y[y != ignore_id] for y in ys_pad]  # parse padded ys
+    ys_len = len(ys)
+
+    ys_in_del = [y.clone() for y in ys]
+    dur_out_del = []
+    for i in range(ys_len):
+        y_len = len(ys[i])
+        num_samples = numpy.random.randint(1, y_len + 1)
+        idx = numpy.random.choice(y_len, num_samples)
+
+        ys_in_del[i][idx] = mask_token
+        
+        unmask_idx = torch.arange(y_len).to(ys[i]).masked_fill((ys_in_del[i] == mask_token), ignore_id)
+        _, dur = unmask_idx.unique_consecutive(return_counts = True)
+
+        ys_in_del[i] = ys_in_del[i][dur.cumsum(0) - 1]
+        dur_out_del.append(dur.masked_fill((ys_in_del[i] != mask_token), ignore_id))
+    
+    # [1,2,3,4] -> [0,1,0,2,0,3,0,4] 
+    ys_in_ins = [torch.stack([y.new(y.size()).fill_(ignore_id), y.clone()]).t().flatten() for y in ys]
+    dur_out_ins = [torch.stack([y.new(y.size()).fill_(0), y.new(y.size()).fill_(1)]).t().flatten() for y in ys]
+    
+    for i in range(len(ys)):
+        ylen = len(ys[i])
+        num_samples = numpy.random.randint(1, ylen + 1)
+        idx = numpy.random.choice(ylen, num_samples) * 2
+
+        # [0,1,0,2,0,3,0,4] ->(random) [0, 1, mask, 2,mask ,3, 0, 4]
+        ys_in_ins[i][idx] = mask_token
+        # [0, 1, mask, 2,mask ,3, 0, 4] -> [1, mask, 2, mask, 3, 4]
+        
+        tgt_idx = torch.where(ys_in_ins[i] != ignore_id)[0] # 取出Mask 跟 非Ignore id
+
+        ys_in_ins[i] = ys_in_ins[i][tgt_idx] # 貼回去，得到有insert mask的sequence
+        dur_out_ins[i] = dur_out_ins[i][tgt_idx] # 對應的位置的數字
+
+    
+    ys_in_del.extend(ys_in_ins)
+    dur_out_del.extend(dur_out_ins)
+
+    return pad_list(ys_in_del, eos), pad_list(dur_out_del, ignore_id)
+
+    
+def shrink(ys_in, mask_token, eos, ignore_id):
+    from espnet.nets.pytorch_backend.nets_utils import pad_list
+    ys_copy = [y.clone() for y in ys_in]
+    ys_num = [y.new(y.size()).fill_(ignore_id) for y in ys_in]
+    ys_shrink = []
+
+    for i in range(len(ys_copy)):
+        shrink , cnt  = ys_copy[i].unique_consecutive(return_counts = True)
+        mask_idx = torch.nonzero(shrink == mask_token)
+        ys_num[i][mask_idx] = cnt[mask_idx]
+        ys_shrink.append(shrink)
+    # # ys_num = [y.new(y.size()).fill_(ignore_id) for y in ys_in] # fill with the number of mask
+    # # ys_shrink = [y.new(y.size()).fill_(eos) for y in ys_in]
+    # for i in range(len(ys_in)):
+    #     have_mask = False
+    #     mask_count = 0
+    #     index = 0
+    #     # logging.info(f'ys_in:{ys_in[i]}')
+    #     for j in range(len(ys_in[i])):
+    #         if (ys_in[i][j] == mask_token):
+    #             mask_count += 1
+    #             have_mask = True
+    #         else :
+    #             if (have_mask):
+    #                 ys_shrink[i][index] = mask_token
+    #                 ys_num[i][index] = mask_count
+    #                 mask_count = 0
+    #                 have_mask = False
+    #                 index += 1
+    #             ys_shrink[i][index] = ys_in[i][j]
+    #             index += 1
+    #         if (have_mask):
+    #             ys_shrink[i][index] = mask_token
+    #             ys_num[i][index] = mask_count
+    return pad_list(ys_shrink, eos), pad_list(ys_num, ignore_id)
+
+def insert_mask(ys_in, mask_token, eos, ignore_id):
+    from espnet.nets.pytorch_backend.nets_utils import pad_list
+    ys_insert = [y.clone() for y in ys_in]
+    ys_insert = [y[y != ignore_id] for y in ys_insert]
+    ys_num = [y.new(y.size()).fill_(ignore_id) for y in ys_in]
+    for i in range(len(ys_insert)):
+        num_samples = numpy.random.randint(1, len(ys_insert[i]) + 1)
+        idx = numpy.random.choice(len(ys_insert[i]), num_samples)
+        
+        for i in range(len(ys_insert)):
+            for j in range(len(ys_insert[i])):
+                if (ys_insert[i][j] == mask_token):
+                    ys_num[i][j] = 1
+
+        # print(f'ys_before_insert:{ys_insert[i]}')
+        # print(f'ys_num_before_ins:{ys_num[i]}')
+        # print(f'idx:{idx}')
+        for k, n in enumerate(idx):
+            # print(n)
+            ys_insert[i] = torch.cat((ys_insert[i][:n], torch.tensor((1, )).fill_(mask_token).to(ys_in.device), ys_insert[i][n:]))
+            ys_num[i] = torch.cat((ys_num[i][:n], torch.zeros((1, )).to(ys_in.device), ys_num[i][n:]))
+        # print(f'ys_insert:{ys_insert[i]}')
+        # print(f'ys_insert_num:{ys_num[i]}')
+    return pad_list(ys_insert, eos), pad_list(ys_num, ignore_id)
+
+
+def expand(ys_in, ys_nums, mask_token, eos, ignore_id):
+    from espnet.nets.pytorch_backend.nets_utils import pad_list
+    ys_expand = [y.clone() for y in ys_in]
+    ys_num = [y.clone() for y in ys_nums]
+    print(ys_num)
+    for i in range(len(ys_num)):
+        ys_temp = [numpy.array([mask_token] * t) if t > 0 else ys_in[i][j].numpy() for j, t in enumerate(ys_num[i])]
+        ys_expand = torch.tensor(numpy.hstack(ys_temp)).unsqueeze(0)
+        # for j, t in enumerate(ys_num[i]):
+        #     if (t == 0):
+        #         ys_expand[i] = torch.cat((ys_expand[i][:j], ys_expand[i][j+1:])) # delete element
+        #     elif (t > 1):
+        #         print(f'before expand: {ys_expand}')
+                
+        #         mask_tensor = torch.zeros([t - 1]).fill_(mask_token).to(ys_in)
+        #         print(mask_tensor.shape)
+        #         ys_expand[i] = torch.cat((ys_expand[i][:j], mask_tensor, ys_expand[i][j:]))
+        #         print(f'after expand : {ys_expand}')
+
+    return pad_list(ys_expand, eos)
